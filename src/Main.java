6480674bb95a2fabf8054e65bc973722f1c1fdb9
Tatiana Kosarevskaya <tv.kosarevskaya@gmail.com>
2021-05-19 17:15:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000; //руб
+        int bonusMiles = 20; //руб
+        int totalBonusMiles = ticketPrice / bonusMiles;
+        System.out.println("Total Bonus Miles:" + totalBonusMiles);
+    }
+}
